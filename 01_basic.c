dbbb65b9047f9659c061f329c54bba0d49979f53
tooksTanmay <97392494+tooksTanmay@users.noreply.github.com>
2022-03-04 20:51:35 +0530

    update1

--- 01_basic.c
+++ 01_basic.c
@@ -1,19 +1,83 @@
-#include<stdio.h>
-#include<math.h>
-#include<stdlib.h>
-#include<time.h>
+#include <stdio.h>
+#include <math.h>
+#include <stdlib.h>
+#include <time.h>
 
+int calculator(float number_1, char operation, float number_2)
+{
+    printf("\n");
+    printf("**This is a calculator**\n");
+    printf("(num1 operation num2 +, -, /, *): ");
+    scanf("%f %c %f", &number_1, &operation, &number_2);
+    switch (operation)
+    {
+    case '+':
+        printf("\n");
+        printf("Answers: %0.3f", number_1 + number_2);
+        printf("\n \n");
+        break;
+    case '-':
+        printf("\n");
+        printf("Answers: %0.3f", number_1 - number_2);
+        printf("\n \n");
+        break;
+    case '*':
+        printf("\n");
+        printf("Answers: %0.3f", number_1 * number_2);
+        printf("\n \n");
+        break;
+    case '/':
+        printf("\n");
+        printf("Answers: %0.3f", number_1 / number_2);
+        printf("\n \n");
+        break;
 
+    default:
+        break;
+    }
+}
+
+void delay(int number_of_seconds)
+{
+    // Converting time into milli_seconds
+    int milli_seconds = 1000 * number_of_seconds;
+  
+    // Storing start time
+    clock_t start_time = clock();
+  
+    // looping till required time is not achieved
+    while (clock() < start_time + milli_seconds)
+        ;
+}
+
+void loop(float number_1, char operation, float number_2)
+{
+    delay(1000);
+    char opinion;
+    printf("Do you wanna continue? (y/n) ");
+    while (opinion)
+    {
+        scanf("%c", &opinion);
+        if (opinion == 'y')
+        {
+            calculator(number_1, operation, number_2);
+            return loop(number_1, operation, number_2);
+        }
+        else if (opinion == 'n')
+        {
+            printf("\n");
+            break;
+        }  
+
+    }
+    return;
+}
 
 int main()
 {
-    int x, y, z, k;
-    x = 2, y = 3, z = 3, k = 1;
-    // 3*x/y-z+k
-    // 3*2/3-3+1
-    // 6/3-3+1
-    // 2-3+1
-    // 0
-    printf("The value of 3*x/y-z+k is %d \n", 3*x/y-z+k);
+    float number_1, number_2;
+    char operation;
+    calculator(number_1, operation, number_2);
+    loop(number_1, operation, number_2);
     return 0;
 }
