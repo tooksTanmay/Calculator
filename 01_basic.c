688357f3a1f2ce4eb71dfd4b171f63169c233b5f
tooksTanmay <97392494+tooksTanmay@users.noreply.github.com>
2022-03-04 20:47:38 +0530

    first

---
+++ 01_basic.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+#include<math.h>
+#include<stdlib.h>
+#include<time.h>
+
+
+
+int main()
+{
+    int x, y, z, k;
+    x = 2, y = 3, z = 3, k = 1;
+    // 3*x/y-z+k
+    // 3*2/3-3+1
+    // 6/3-3+1
+    // 2-3+1
+    // 0
+    printf("The value of 3*x/y-z+k is %d \n", 3*x/y-z+k);
+    return 0;
+}
